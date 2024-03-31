63d298a576710e8d1614c29626569b27688cd34a
atlfsj <3321713512@qq.com>
2024-03-31 16:41:27 +0800

    继续测试相册功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.less'
+import ImageCropper from 'vue-image-crop-upload';
 
 const app = createApp(App)
-app.use(router).mount('#app')
+app.use(router).component('image-cropper', ImageCropper).mount('#app')
